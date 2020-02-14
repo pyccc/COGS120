a5ea5609a886b8b075a72f066d9a362e96005691
pyccc <42711563+pyccc@users.noreply.github.com>
2020-02-13 16:05:52 -0800

    structure

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-var PORT = 3000;
+/**
+ * Module dependencies.
+ */
 
-// Express is a web framework for node.js
-// that makes nontrivial applications easier to build
 var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
 
-// Create the server instance
+var index = require('./routes/index');
+var AltExercises = require('./routes/AltExercises');
+var Breathing = require('./routes/Breathing');
+var ChooseExercise = require('./routes/ChooseExercise');
+var Done = require('./routes/Done');
+var chooseEmotion = require('./routes/chooseEmotion');
+var settings = require('./routes/settings');
+
 var app = express();
 
-// Print logs to the console and compress pages we send
-app.use(express.logger());
-app.use(express.compress());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Return all pages in the /static directory
-// whenever they are requested at '/'
-// e.g., http://localhost:3000/index.html
-// maps to /static/index.html on this machine
-app.use(express.static(__dirname + '/static'));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-// Start the server
-var port = process.env.PORT || PORT; // 80 for web, 3000 for development
-app.listen(port, function() {
-	console.log("Node.js server running on port %s", port);
+// Add routes here
+app.get('/', index.view);
+app.get('/AltExercises', AltExercises.view);
+app.get('/Breathing', Breathing.view);
+app.get('/ChooseExercise', ChooseExercise.view);
+app.get('/Done', Done.view);
+app.get('/chooseEmotion', chooseEmotion.view);
+app.get('/settings', settings.view);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
