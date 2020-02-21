c04df7253d97dcf70ccade45bac96b5147b91380
Marlon Cortez <marloncortez_99@hotmail.com>
2020-02-20 21:01:49 -0800

    Auditory and data.json fixes

--- routes/Auditory.js
+++ routes/Auditory.js
@@ -1,3 +1,32 @@
-exports.view = function(req, res){
-  res.render('Auditory');
+var data = {
+  "emotions": [
+    {
+      "name": "morehappy",
+      "musicURL": "https://open.spotify.com/embed/playlist/37i9dQZF1DX5YTAi6JhwZm"
+    },
+    {
+      "name": "neutral",
+      "musicURL": "https://open.spotify.com/embed/playlist/37i9dQZF1DX4PP3DA4J0N8"
+    }
+  ]
+};
+
+exports.projectInfo = function(request, response) {
+	var emotionsID = request.params.name;
+	var emotions = getProjectData(emotionsID);
+  	response.json(emotions);
+};
+exports.view = function(request, response) {
+	//var emotionsID = request.params.id;
+	//var emotion = getProjectData(emotionsID);
+	response.render('Auditory', data);
+};
+function getProjectData() {
+	if (emotionsID == "random") {
+		emotionsID = Math.floor(Math.random() * data.length) + 1;
+	} else {
+		emotionsID = parseInt(emotionsID);
+	}
+  var emotions = data[emotionsID-1]; // of by one, our first project has index 0
+  return emotions;
 };
--- routes/emotionExercise.js
+++ routes/emotionExercise.js
@@ -1,8 +1,4 @@
-var data = require('../static/data.json');
-/*
- * GET home page.
- */
 
 exports.view = function(req, res){
-  res.render('emotionExercise',data);
+  res.render('emotionExercise');
 };
