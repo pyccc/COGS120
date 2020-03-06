138e41879306cb218eca3293db623de6a8de3021
Zetroc99 <49223290+Zetroc99@users.noreply.github.com>
2020-03-05 22:46:46 -0800

    added images and proper music for each emotion

--- routes/Auditory.js
+++ routes/Auditory.js
@@ -1,31 +1,21 @@
-var data = {
-  "emotions": [
-    {
-      "name": "morehappy",
-      "musicURL": "https://open.spotify.com/embed/playlist/37i9dQZF1DX5YTAi6JhwZm"
-    },
-    {
-      "name": "neutral",
-      "musicURL": "https://open.spotify.com/embed/playlist/37i9dQZF1DX4PP3DA4J0N8"
+var data = require('../static/data.json');
+
+exports.view = function(req, res) {
+
+  data.emotions.forEach(emo=>{
+    if(emo.name===req.params.type){
+      emotion=emo;
     }
-  ]
-};
-
-exports.projectInfo = function(request, response) {
-	var emotionsID = request.params.name;
-	var emotions = getProjectData(emotionsID);
-  	response.json(emotions);
-};
-exports.view = function(request, response) {
-	//var emotionsID = request.params.id;
-	//var emotion = getProjectData(emotionsID);
+  })
   data['versionB'] = false;
-  response.render('Auditory', data);
+  res.render('Auditory', emotion);
 };
-exports.auditory_B = function(request, response) {
-	//var emotionsID = request.params.id;
-	//var emotion = getProjectData(emotionsID);
+exports.auditory_B = function(req, res) {
+  data.emotions.forEach(emo=>{
+    if(emo.name===req.params.type){
+      emotion=emo;
+    }
+  })
   data['versionB'] = true;
-  response.render('Auditory', data);
+  res.render('Auditory', emotion);
 };
-
